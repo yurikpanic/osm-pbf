21bb4be5e592657c096da8e073025bcc5785b2aa
Iurii Vishnevskyi <vishnevsky@gmail.com>
2012-08-23 13:38:45 +0000

    objects are not loaded into mem - just read from file, the will not fiy in memory. So we will read them and perform some actions on them. Added the ability to skip blocks in osm.pbf file and to process fixed amount of blocks.
    TODO: read relations and study how we can use sens to reverce geocode current location
    
    git-svn-id: svn://elsyton.com/var/lib/svn/globus/osm-pbf/trunk@4664 51761ecd-e902-0410-a4fa-e0faf22394c5

--- osm-reader.lisp
+++ osm-reader.lisp
@@ -11,53 +11,54 @@
         (format stream " zlib data (size ~A, uncompressed ~A)" (length (osmpbf:zlib-data blob)) (osmpbf:raw-size blob))))
   (format stream "~%"))
 
-(defun read-osm-file (&optional (file-name #p"/home/yuri/work/globus/osm/UA.osm.pbf"))
+(defvar *last-block-processed* nil)
+
+(defun read-osm-file (&key (file-name #p"/home/yuri/work/globus/osm/UA.osm.pbf") (skip-to-block 0) count)
   (with-open-file (fs file-name :direction :input :element-type 'unsigned-byte)
     (do ((i 0 (1+ i))
          (file-size (file-length fs))
          (file-pos 0 (file-position fs))
-         (nodes nil))
-        ((>= file-pos file-size) (values nodes))
+         (proc-count 0))
+        ((>= file-pos file-size) nil)
       (let ((blob-header-len-buf (make-array 4 :element-type '(unsigned-byte 8))))
         (read-sequence blob-header-len-buf fs)
-        (format t "================================= :~A [~,1F %]~%blob-header-len-buf ~A~%" file-pos (* (/ file-pos file-size) 100.0) blob-header-len-buf)
+        ;;(format t "================================= :~A [~,1F %]~%blob-header-len-buf ~A~%" file-pos (* (/ file-pos file-size) 100.0) blob-header-len-buf)
         (let ((blob-header-len 0))
           (loop for d across blob-header-len-buf
              do (setf blob-header-len (logior (ash blob-header-len 8) d)))
-          (format t "blob-header-len ~A~%" blob-header-len)
+          ;;(format t "blob-header-len ~A~%" blob-header-len)
           (let ((blob-header-buf (make-array blob-header-len :element-type '(unsigned-byte 8))))
             (read-sequence blob-header-buf fs)
             (let ((blob-header (make-instance 'osmpbf:blob-header)))
               (pb:merge-from-array blob-header blob-header-buf 0 blob-header-len)
-              (format t "blob-header ~A~%" blob-header)
-              (let ((blob-buf (make-array (osmpbf:datasize blob-header) :element-type '(unsigned-byte 8))))
-                (read-sequence blob-buf fs)
-                (let ((blob (make-instance 'osmpbf:blob)))
-                  (pb:merge-from-array blob blob-buf 0 (length blob-buf))
-                  (print-blob-descr blob)
-                  (handler-case
-                      (let ((data (if (osmpbf:has-raw blob)
-                                      (osmpbf:raw blob)
-                                      (coerce (zlib:uncompress (osmpbf:zlib-data blob) :uncompressed-size (osmpbf:raw-size blob)) '(simple-array (unsigned-byte 8) (*))))))
-                        (cond
-                          ((string= (pb:string-value (osmpbf:type blob-header)) "OSMHeader")
-                           (read-osm-header data))
-                          ((string= (pb:string-value (osmpbf:type blob-header)) "OSMData")
-                           (setf nodes (read-osm-data data i nodes))
-                           (when (>= i 10)
-                             (return-from read-osm-file (values nodes)))
-                           )))
-                    (condition (c) (format t "!!! data decode error ~A~%" c))))))))))))
-
-;; (defparameter *nodes* (read-osm-file))
-;; (dolist (nn *nodes*)
-;;   (loop for node across nn
-;;      do (print-node node)))
+              ;;(format t "blob-header ~A ~A~%" i blob-header)
+              (if (< i skip-to-block)
+                  (file-position fs (+ (file-position fs) (osmpbf:datasize blob-header)))
+                  (let ((blob-buf (make-array (osmpbf:datasize blob-header) :element-type '(unsigned-byte 8))))
+                    (setf *last-block-processed* i)
+                    (read-sequence blob-buf fs)
+                    (let ((blob (make-instance 'osmpbf:blob)))
+                      (pb:merge-from-array blob blob-buf 0 (length blob-buf))
+                      ;;(print-blob-descr blob)
+                      (handler-case
+                          (let ((data (if (osmpbf:has-raw blob)
+                                          (osmpbf:raw blob)
+                                          (coerce (zlib:uncompress (osmpbf:zlib-data blob) :uncompressed-size (osmpbf:raw-size blob)) '(simple-array (unsigned-byte 8) (*))))))
+                            (cond
+                              ((string= (pb:string-value (osmpbf:type blob-header)) "OSMHeader")
+                               (read-osm-header data))
+                              ((string= (pb:string-value (osmpbf:type blob-header)) "OSMData")
+                               (read-osm-data data)
+                               (incf proc-count)
+                               (when (and count (>= proc-count count))
+                                 (return-from read-osm-file)))))
+                        (condition (c) (format t "!!! data decode error ~A~%" c)))))))))))))
 
 (defun read-osm-header (data)
   (let ((header (make-instance 'osmpbf:header-block)))
     (pb:merge-from-array header data 0 (length data))
-    (format t "header ======~%~A~%" header)))
+    ;; (format t "header ======~%~A~%" header)
+    ))
 
 (defun read-string-table (st)
   (let ((st-converted (make-array (length st))))
@@ -73,16 +74,18 @@
   (id 0 :type (unsigned-byte 64))
   (lon 0 :type (unsigned-byte 64))
   (lat 0 :type (unsigned-byte 64))
-  (st-ref #() :type array)
   (tags nil :type list))
 
-(defun print-node (node &optional (stream t))
-  (format stream ":id ~A :lon ~A :lat ~A" (node-id node) (node-lon node) (node-lat node))
-  (dolist (tag (node-tags node))
-    (format stream "~%    ~A . ~A"
-            (aref (node-st-ref node) (car tag))
-            (aref (node-st-ref node) (cdr tag))))
-  (format stream "~%"))
+(defstruct way
+  (id 0 :type (unsigned-byte 64))
+  (refs (make-array 0 :element-type '(unsigned-byte 64)) :type (simple-array (unsigned-byte 64) (*)))
+  (tags nil :type list))
+
+(defun find-tag (tags key)
+  (dolist (tag tags)
+    (when (string= (car tag) key)
+      (return-from find-tag tag)))
+  nil)
 
 (defun read-keys-values-arr (kv-arr st-arr nodes)
   "read key-value array and populate unpacked nodes with their attributes"
@@ -97,8 +100,10 @@
           (let ((value (aref kv-arr kv-idx)))
             (incf kv-idx)
             (let ((node (aref nodes node-idx)))
-              (setf (node-st-ref node) st-arr
-                    (node-tags node) (cons (cons key value) (node-tags node)))))))))
+              (setf (node-tags node)
+                    (cons (cons (aref st-arr key)
+                                (aref st-arr value))
+                          (node-tags node)))))))))
 
 (defun unpack-dense (dn st-arr)
   (let ((prev-node nil)
@@ -121,31 +126,68 @@
     (read-keys-values-arr (osmpbf:keys-vals dn) st-arr nodes)
     nodes))
 
-(defun read-osm-data (data idx &optional nodes)
+(defun read-ways (ways st-arr)
+  (let ((new-ways (make-array (length ways) :element-type 'way :initial-element (make-way))))
+    (loop for way across ways
+         for i from 0
+         do (let ((new-way (make-way :id (osmpbf:id way)
+                                 :refs (make-array (length (osmpbf:refs way)) :element-type '(unsigned-byte 64) :initial-element 0)))
+                  (prev-ref 0))
+              (loop for rr across (osmpbf:refs way)
+                   for j from 0
+                   do (let ((new-ref (+ prev-ref rr)))
+                        (setf (aref (way-refs new-way) j) new-ref
+                              prev-ref new-ref)))
+              (loop for key across (osmpbf:keys way)
+                   for val across (osmpbf:vals way)
+                   do (setf (way-tags new-way)
+                            (cons (cons (aref st-arr key) (aref st-arr val))
+                                  (way-tags new-way))))
+              (let ((name (cdr (find-tag (way-tags new-way) "name"))))
+                (when (and (find-tag (way-tags new-way) "boundary")
+                           (not (find-tag (way-tags new-way) "waterway"))
+                           (> (length (way-tags new-way)) 2))
+                  (when (= (aref (way-refs new-way) 0)
+                           (aref (way-refs new-way) (1- (length (way-refs new-way)))))
+                    (format t "===:="))
+                  (let ((*print-pretty* nil))
+                    (format t "~A ~A~%" name (way-tags new-way)))))
+              (setf (aref new-ways i) new-way)))
+    new-ways))
+
+(defvar *last-dense* nil)
+(defvar *last-ways* nil)
+(defvar *last-string-table* nil)
+
+(defun read-osm-data (data)
   (let ((pblock (make-instance 'osmpbf:primitive-block)))
     (pb:merge-from-array pblock data 0 (length data))
-    (format t "pblock ~A ======~%" idx)
-    (format t "granularity ~A~%" (osmpbf:granularity pblock))
-    (format t "lat-offset ~A~%" (osmpbf:lat-offset pblock))
-    (format t "lon-offset ~A~%" (osmpbf:lon-offset pblock))
+    ;; (format t "pblock ======~%")
+    ;; (format t "granularity ~A~%" (osmpbf:granularity pblock))
+    ;; (format t "lat-offset ~A~%" (osmpbf:lat-offset pblock))
+    ;; (format t "lon-offset ~A~%" (osmpbf:lon-offset pblock))
     (let ((string-table (read-string-table (osmpbf:s (osmpbf:stringtable pblock)))))
-      (format t "string-table with ~A entries~%" (length string-table))
+      ;;(format t "string-table with ~A entries~%" (length string-table))
+      (setf *last-string-table* string-table)
       (let ((pgroup-arr (osmpbf:primitivegroup pblock)))
         (loop for pgroup across pgroup-arr
            for pgidx from 0
            do
-             (format t " pgroup ~A ==~%  nodes ~A~%  dense ~A~%  ways ~A~%  relations ~A~%  changesets ~A~%"
-                     pgidx
-                     (length (osmpbf:nodes pgroup))
-                     (if (osmpbf:has-dense pgroup)
-                         (progn
-                           (setf nodes
-                                 (cons
-                                  (unpack-dense (osmpbf:dense pgroup) string-table)
-                                  nodes))
-                           (length (osmpbf:id (osmpbf:dense pgroup))))
-                         0)
-                     (length (osmpbf:ways pgroup))
-                     (length (osmpbf:relations pgroup))
-                     (length (osmpbf:changesets pgroup))))
-        nodes))))
+             (progn
+               (when (osmpbf:has-dense pgroup)
+                 (setf *last-dense* (osmpbf:dense pgroup)))
+               (unless (zerop (length (osmpbf:ways pgroup)))
+                 (setf *last-ways* (osmpbf:ways pgroup)))
+               
+               (read-ways (osmpbf:ways pgroup) string-table)
+               
+               ;; (format t " pgroup ~A ==~%  nodes ~A~%  dense ~A~%  ways ~A~%  relations ~A~%  changesets ~A~%"
+               ;;         pgidx
+               ;;         (length (osmpbf:nodes pgroup))
+               ;;         (if (osmpbf:has-dense pgroup)
+               ;;             (length (osmpbf:id (osmpbf:dense pgroup)))
+               ;;             0)
+               ;;         (length (osmpbf:ways pgroup))
+               ;;         (length (osmpbf:relations pgroup))
+               ;;         (length (osmpbf:changesets pgroup)))
+               ))))))

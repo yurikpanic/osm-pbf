3debb619185be83d448e5493aa1cfd8dc85b6262
Iurii Vishnevskyi <vishnevsky@gmail.com>
2012-09-19 14:19:46 +0000

    Implemented the search of nodes in btree stored on disk without loading any blobs (nodes or btree) to memory
    git-svn-id: svn://elsyton.com/var/lib/svn/globus/osm-pbf/trunk@4702 51761ecd-e902-0410-a4fa-e0faf22394c5

--- osm-index-search.lisp
+++ osm-index-search.lisp
@@ -8,7 +8,15 @@
   (file-name #p"/home/yuri/work/globus/osm/out.osm.pbf" :type pathname)
   (blob-offsets (make-array 0 :element-type '(unsigned-byte 64) :adjustable t :fill-pointer 0) :type vector)
   (node-id-btree (make-instance 'btreepbf:btree) :type btreepbf:btree)
+  (deserialize-array nil :type (or null vector))
   (btree-offset 0 :type integer))
+
+(defun get-deserialize-array (sd size)
+  (let ((arr (search-data-deserialize-array sd)))
+    (when (or (not arr) (< (array-dimension arr 0) size))
+      (setf arr (make-array size :element-type '(unsigned-byte 8))
+            (search-data-deserialize-array sd) arr))
+    arr))
 
 (defmacro read-uint32 (fs)
   (let ((buf (gensym))
@@ -35,24 +43,6 @@
               (pb:merge-from-array blob-header blob-header-buf 0 blob-header-len)
               blob-header)))))
 
-;; (defun load-blob-and-header (fs &optional pos)
-;;   (when pos
-;;     (file-position fs pos))
-;;   (let ((blob-header-len-buf (make-array 4 :element-type '(unsigned-byte 8))))
-;;     (read-sequence blob-header-len-buf fs)
-;;     (let ((blob-header-len 0))
-;;       (loop for d across blob-header-len-buf
-;;          do (setf blob-header-len (logior (ash blob-header-len 8) d)))
-;;       (let ((blob-header-buf (make-array blob-header-len :element-type '(unsigned-byte 8))))
-;;             (read-sequence blob-header-buf fs)
-;;             (let ((blob-header (make-instance 'osmpbf:blob-header)))
-;;               (pb:merge-from-array blob-header blob-header-buf 0 blob-header-len)
-;;               (let ((blob-buf (make-array (osmpbf:datasize blob-header) :element-type '(unsigned-byte 8))))
-;;                 (read-sequence blob-buf fs)
-;;                 (let ((blob (make-instance 'osmpbf:blob)))
-;;                   (pb:merge-from-array blob blob-buf 0 (length blob-buf))
-;;                   (values blob-header blob))))))))
-
 (defun get-blob-uncompressed-data (blob)
   (if (osmpbf:has-raw blob)
       (osmpbf:raw blob)
@@ -72,7 +62,6 @@
     (with-open-file (fs (search-data-file-name sd) :direction :input :element-type '(unsigned-byte 8))
       (map-blobs #'(lambda (header header-pos)
                      (vector-push-extend header-pos (search-data-blob-offsets sd))
-                     (format t "~A ~A~%" header header-pos)
                      (when (string= (pb:string-value (osmpbf:type header)) "btree")
                        (let* ((btree-header-len (read-uint32 fs))
                               (buf (make-array btree-header-len :element-type '(unsigned-byte 8))))
@@ -86,114 +75,68 @@
                  fs))
     sd))
 
-;; (defun init-osm-search (&optional (file-name #p"/home/yuri/work/globus/osm/out.osm.pbf"))
-;;   (let ((sd (make-search-data :file-name file-name)))
-;;     (with-open-file (f (search-data-file-name sd) :direction :input :element-type '(unsigned-byte 8))
-;;       (map-blobs #'(lambda (header data header-pos)
-;;                      (vector-push-extend header-pos (search-data-blob-offsets sd))
-;;                      (when (string= (pb:string-value (osmpbf:type header)) "btree")
-;;                        (let ((btree (make-instance 'btreepbf:btree)))
-;;                          (pb:merge-from-array btree data 0 (length data))
-;;                          (when (and (string= (pb:string-value (btreepbf:type btree)) "node")
-;;                                     (string= (pb:string-value (btreepbf:field btree)) "id"))
-;;                            (setf (search-data-node-id-btree sd) btree
-;;                                  (search-data-btree-offset sd) (file-position f))))))
-;;                  f))
-;;     sd))
+(defun get-btree-node (sd offs size)
+  (with-open-file (fs (search-data-file-name sd) :direction :input :element-type '(unsigned-byte 8))
+    (file-position fs (+ offs (search-data-btree-offset sd)))
+    (let ((arr (get-deserialize-array sd size)))
+      (read-sequence arr fs :end size)
+      (let ((bnode (make-instance 'btreepbf:bnode)))
+        (pb:merge-from-array bnode arr 0 size)
+        bnode))))
 
-;; (defvar *blob-cache* (make-hash-table :test 'eq))
+(defun search-btree (sd bnode id)
+  (let ((keys-len (length (btreepbf:keys bnode))))
+    (if (= (btreepbf:kind bnode) btreepbf:+bnode-kind-node+)
+        (let ((descend-idx (if (< id (aref (btreepbf:keys bnode) 0))
+                               0
+                               (or 
+                                (block k-search
+                                  (dotimes (i (1- keys-len))
+                                    (when (and (<= (aref (btreepbf:keys bnode) i) id) (< id (aref (btreepbf:keys bnode) (1+ i))))
+                                      (return-from k-search (1+ i))))
+                                  nil)
+                                keys-len))))
+          (search-btree
+           sd
+           (get-btree-node sd
+                           (aref (btreepbf:pointers bnode) descend-idx)
+                           (aref (btreepbf:child-sizes bnode) descend-idx))
+           id))
+        (progn
+          (loop for key across (btreepbf:keys bnode)
+             for val across (btreepbf:values bnode)
+               do (when (= key id)
+                    (return-from search-btree val)))
+          nil))))
 
-;; (defun get-btree-entry-in-blob (sd blob-num idx)
-;;   (let ((btree (gethash blob-num *blob-cache*)))
-;;     (unless btree
-;;       (with-open-file (fs (search-data-file-name sd) :direction :input :element-type '(unsigned-byte 8))
-;;         (let ((blob-offset (aref (search-data-blob-offsets sd) blob-num)))
-;;           (multiple-value-bind (header blob)
-;;               (load-blob-and-header fs blob-offset)
-;;             (when (string= (pb:string-value (osmpbf:type header)) "btree")
-;;               (let ((data (get-blob-uncompressed-data blob)))
-;;                 (setf btree (make-instance 'btreepbf:btree))
-;;                 (pb:merge-from-array btree data 0 (length data))
-;;                 (setf (gethash blob-num *blob-cache*) btree)))))))
-;;     (aref (btreepbf:nodes btree) idx)))
+(defun load-node-direct (sd blob-num offs node-size)
+  (with-open-file (fs (search-data-file-name sd) :element-type '(unsigned-byte 8))
+    (file-position fs (aref (search-data-blob-offsets sd) blob-num))
+    (let ((blob-header-len-buf (make-array 4 :element-type '(unsigned-byte 8))))
+      (read-sequence blob-header-len-buf fs)
+      (let ((blob-header-len 0))
+        (loop for d across blob-header-len-buf
+           do (setf blob-header-len (logior (ash blob-header-len 8) d)))
+        (let ((blob-header-buf (make-array blob-header-len :element-type '(unsigned-byte 8))))
+          (read-sequence blob-header-buf fs)
+          (file-position fs (+ (file-position fs) offs))
+          (let ((node-buf (make-array node-size :element-type '(unsigned-byte 8))))
+            (read-sequence node-buf fs)
+            node-buf))))))
 
-;; (defun get-btree-entry (sd index)
-;;   ;; when there will be multiple btree indexes - add here parameters to select proper btree (e.g. by struc type and field name)
-;;   (loop for entry across (btreepbf:entries (search-data-node-id-btree-idx sd))
-;;        do
-;;        (when (and (>= index (btreepbf:min-node entry)) (<= index (btreepbf:max-node entry)))
-;;          (return-from get-btree-entry (get-btree-entry-in-blob sd (btreepbf:blob-num entry) (- index (btreepbf:min-node entry)))))))
-
-;; (defun search-btree (sd bnode id)
-;;   (let ((keys-len (length (btreepbf:keys bnode))))
-;;     (if (= (btreepbf:kind bnode) btreepbf:+bnode-kind-node+)
-;;         (search-btree
-;;          sd
-;;          (get-btree-entry
-;;           sd
-;;           (aref (btreepbf:pointers bnode)
-;;                 (if (< id (aref (btreepbf:keys bnode) 0))
-;;                     0
-;;                     (or 
-;;                      (block k-search
-;;                        (dotimes (i (1- keys-len))
-;;                          (when (and (<= (aref (btreepbf:keys bnode) i) id) (< id (aref (btreepbf:keys bnode) (1+ i))))
-;;                            (return-from k-search (1+ i))))
-;;                        nil)
-;;                      keys-len))))
-;;          id)
-;;         (progn
-;;           (loop for key across (btreepbf:keys bnode)
-;;              for val across (btreepbf:values bnode)
-;;                do (when (= key id)
-;;                     (return-from search-btree val)))
-;;           nil))))
-
-;; (defun load-node-direct (sd blob-num offs node-size)
-;;   (with-open-file (fs (search-data-file-name sd) :element-type '(unsigned-byte 8))
-;;     (file-position fs (aref (search-data-blob-offsets sd) blob-num))
-;;     (let ((blob-header-len-buf (make-array 4 :element-type '(unsigned-byte 8))))
-;;       (read-sequence blob-header-len-buf fs)
-;;       (let ((blob-header-len 0))
-;;         (loop for d across blob-header-len-buf
-;;            do (setf blob-header-len (logior (ash blob-header-len 8) d)))
-;;         (let ((blob-header-buf (make-array blob-header-len :element-type '(unsigned-byte 8))))
-;;           (read-sequence blob-header-buf fs)
-;;           (file-position fs (+ (file-position fs) offs))
-;;           (let ((node-buf (make-array node-size :element-type '(unsigned-byte 8))))
-;;             (read-sequence node-buf fs)
-;;             node-buf))))))
-
-;; (defun get-node-entry-in-blob (sd blob-num idx &optional offs size)
-;;   (if (and offs size)
-;;       (load-node-direct sd blob-num offs size)
-;;       (let ((pblock (gethash blob-num *blob-cache*)))
-;;         (unless pblock
-;;           (with-open-file (fs (search-data-file-name sd) :element-type '(unsigned-byte 8))
-;;             (multiple-value-bind (header blob)
-;;                 (load-blob-and-header fs (aref (search-data-blob-offsets sd) blob-num))
-;;               (when (string= (pb:string-value (osmpbf:type header)) "OSMData")
-;;                 (let ((data (get-blob-uncompressed-data blob)))
-;;                   (setf pblock (make-instance 'osmpbf:primitive-block))
-;;                   (pb:merge-from-array pblock data 0 (length data))
-;;                   (setf (gethash blob-num *blob-cache*) pblock))))))
-;;         (let ((idx-to-return idx))
-;;           (loop for pgroup across (osmpbf:primitivegroup pblock)
-;;              do (let ((cur-nodes-len (length (osmpbf:nodes pgroup))))
-;;                   (if (< idx-to-return cur-nodes-len)
-;;                       (return-from get-node-entry-in-blob (aref (osmpbf:nodes pgroup) idx-to-return))
-;;                       (decf idx-to-return cur-nodes-len))))))))
-
-;; (defun find-node-by-id (sd id)
-;;   (let* ((root (get-btree-entry sd 0))
-;;          (val (search-btree sd root id)))
-;;     (when val
-;;       (let ((blob-idx (make-instance 'btreepbf:blob-index)))
-;;         (pb:merge-from-array blob-idx val 0 (length val))
-;;         (format t "~A~%" blob-idx)
-;;         (if (and (btreepbf:has-blob-offs blob-idx) (btreepbf:has-size blob-idx))
-;;             (get-node-entry-in-blob sd (btreepbf:blob-num blob-idx) (btreepbf:blob-elt blob-idx) ;;(btreepbf:blob-offs blob-idx) (btreepbf:size blob-idx)
-;;                                     )
-;;             (get-node-entry-in-blob sd (btreepbf:blob-num blob-idx) (btreepbf:blob-elt blob-idx)))))))
+(defun find-node-by-id (sd id)
+  (let* ((btree (search-data-node-id-btree sd))
+         (root (get-btree-node sd (btreepbf:root-offs btree) (btreepbf:root-size btree)))
+         (val (search-btree sd root id)))
+    (when val
+      (let ((blob-idx (make-instance 'btreepbf:blob-index))
+            (node (make-instance 'osmpbf:node)))
+        (pb:merge-from-array blob-idx val 0 (length val))
+        (pb:merge-from-array
+         node
+         (load-node-direct sd (btreepbf:blob-num blob-idx) (btreepbf:blob-offs blob-idx) (btreepbf:size blob-idx))
+         0 (btreepbf:size blob-idx))
+        node))))
 
 ;; e.g. (find-node-by-id *sd* 337732605)
+;;      (find-node-by-id *sd* 337526439)
